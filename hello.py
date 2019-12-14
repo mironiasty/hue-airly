7b94d820b0621be958dc1523e7099ab70a8cc049
Miron Pawlik <miron.pawlik@swmansion.com>
2019-12-14 19:27:08 +0100

    set colors

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 import requests
-import math
 from apikeys import hueuser
-
+from colorConverter import RGBtoXY
 
 # r = requests.post('http://192.168.7.28/api/',
 #                   json={'devicetype': 'hue-airly-app'})
 
 # print(r.status_code)
 # print(r.json())
-r = requests.get('http://192.168.7.28/api/'+hueuser+'/lights/')
-print(r.status_code)
-print(r.json())
+# r = requests.get('http://192.168.7.28/api/'+hueuser+'/lights/14')
+xh = RGBtoXY(211, 34, 11)
+r = requests.put(
+    'http://192.168.7.28/api/'+hueuser+'/lights/14/state', json={'on': True, 'bri': 10, 'xy': xh})
+# print(colorsys.rgb_to_hls(100, 134, 122))
+# print(r.status_code)
+# print(r.json())
